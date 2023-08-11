3500a44b230f68014bd1114af73ad39c5d8f8aa7
Dariusz Zając <darekdmc@gmail.com>
2023-08-11 12:05:31 +0200

    Projekt C# podstawy 21

---
+++ Projektl1d5/Projektl1d5/Program.cs
@@ -0,0 +1,17 @@
+﻿var name = "Ewa";
+var gender = "Kobieta";
+var age = 33;
+
+if (gender == "Kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (gender != "Kobieta" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+// Program wyświetla prawidłowe komunikaty przy deklaracji różnych zmiennych
