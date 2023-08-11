67c20de9daf311c92c4958593984f9ba126880a6
Dariusz Zając <darekdmc@gmail.com>
2023-08-11 14:22:02 +0200

    Initial do zadania

---
+++ Projektl1d5a/Projektl1d5a/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Przygotuj program, który policzy ile jakich cyfr występuje w podanej liczbie:
+Przykład dla liczby 4566:
+0 => 0
+1 => 0
+2 => 0
+3 => 0
+4 => 1
+5 => 1
+6 => 2
+7 => 0
+8 => 0
+9 => 0
+*/
+int number = 4566;
+string numberInString = number.ToString();
+char[] letters = numberInString.ToArray();
